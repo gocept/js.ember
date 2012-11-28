8f7bae88bf27505b7ea99b0155e692938cdf66b9
nilo <nilo@localhost>
2012-11-28 10:53:58 +0100

    add handlebars

--- js/ember/__init__.py
+++ js/ember/__init__.py
@@ -3,5 +3,7 @@ import js.jquery
 
 library = fanstatic.Library('ember', 'resources')
 
+hb = fanstatic.Resource(library, 'handlebars-1.0.0.beta.6.js')
+
 ember = fanstatic.Resource(library, 'ember.js', minified='ember.min.js',
-                 depends=[js.jquery.jquery])
+                 depends=[js.jquery.jquery, hb])
