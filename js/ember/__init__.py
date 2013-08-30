77fb92ce96fc5b19a976b860f42b4974f1abb99e
Sebastian Wehrmann <sw@gocept.com>
2013-08-30 11:38:59 +0200

    Made ember data an independent resource, so that requiering ember does not load emberdata automatically. To get emberdata, use ``js.ember.ember_data.need()``.

--- js/ember/__init__.py
+++ js/ember/__init__.py
@@ -8,13 +8,14 @@ handlebars = fanstatic.Resource(
     'handlebars-1.0.0.js',
     minified='handlebars-1.0.0.min.js')
 
-emberdata = fanstatic.Resource(
-    library,
-    'ember-data-0.13-108.js',
-    minified='ember-data-0.13-108.min.js')
-
 ember = fanstatic.Resource(
     library,
     'ember-1.0.0-rc.8.js',
     minified='ember-1.0.0-rc.8.min.js',
-    depends=[js.jquery.jquery, handlebars, emberdata])
+    depends=[js.jquery.jquery, handlebars])
+
+ember_data = fanstatic.Resource(
+    library,
+    'ember-data-0.13-108.js',
+    minified='ember-data-0.13-108.min.js',
+    depends=[ember])
