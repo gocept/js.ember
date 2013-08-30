49434dce24b2fea59da360e410b47ae0ff437b1d
Sebastian Wehrmann <sebastian@wehrmann.de>
2013-08-30 12:01:26 +0200

    Preparing release 1.0.0-pre.8.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.0-pre.9.dev0'
+version = '1.0.0-pre.8.1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
